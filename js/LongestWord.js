384c2a5454923e59faf9648e79733acf4c17389d
Gitthere <ichi4g@gmail.com>
2014-06-07 19:32:38 -1000

    updated

--- js/LongestWord.js
+++ js/LongestWord.js
@@ -1,26 +1,39 @@
-var button = document.getElementById('getLongestWord');
+(function() {
 
-button.onclick = function checkInput() {
-var wordCheck = document.getElementById('userInput')
-var letters = /^[A-Za-z]+$/; 
-  //check if value is a string of letters
-  if (wordCheck.value.match(letters)) {
-    calcLength();
-  } else {
-    alert('Please enter text only.');
+  var button = document.getElementById('getLongestWord');
+
+  button.onclick = function checkInput() {
+    var wordCheck = document.getElementById('userInput').value;
+    var letters = /^[A-Z a-z]+$/; 
+    var whiteSpace = /\s/g;
+    //check if value is a string of letters or sentence
+    //console.log((wordCheck.match(letters)));
+    if (wordCheck.match(whiteSpace) && wordCheck.match(letters)) {
+      calcLength(wordCheck);
+    } else {
+      alert('Please enter text only.');
+    }
   }
-}
   
 //FUNCTION TO CALC LENGTH OF EACH WORD AND COMPARE EACH WORD
-function calcLength() {
-  var sentence = document.getElementById('userInput');
+  function calcLength(wordCheck) {
+    var words = wordCheck.split(" ");
+    //console.log(words);
+    //var wordsInSentence = words;
+    var longestWord = " ";
 
-}
+    for (i = 0; i < words.length; i++) {
+      //var stringLength = wordsInSentence[i].length;
+      if (words[i].length > longestWord.length) {
+          longestWord = words[i]; 
+      }
 
+    }
+    console.log(longestWord);
+    //console.log(stringLength);
+    
+  }
+})();
 
-//IF EQUAL RETURN BOTH
-
-
-//RETURN LONGEST WORD
 
 
